2ce45865487bb9a41c2d52acd2db38044438e272
Swayam Samyak Debasis <you@example.com>
2020-06-07 00:33:52 +0530

    database complete

--- public/chat.js
+++ public/chat.js
@@ -9,6 +9,24 @@ var btn = document.getElementById('send');
 var output = document.getElementById('output');
 var feedback = document.getElementById('feedback');
 
+fetch("http://localhost:4000/chats/")
+.then(data => {
+  return data.json();
+})
+//   .then(data => {
+//     output.innerHTML +='<p><strong>'+ data.sender + ':</strong>' + data.message + '</p>'
+//   })
+.then(json => {
+  json.map(data => {
+    let li = document.createElement("li");
+    let span = document.createElement("span");
+    messages.appendChild(li).append(data.message);
+    messages
+      .appendChild(span)
+      .append("by " + data.sender + ": " + formatTimeAgo(data.createdAt));
+  });
+});
+
 //Emit events
 
 btn.addEventListener('click',function(){
@@ -18,6 +36,9 @@ btn.addEventListener('click',function(){
         handle:handle.value
     })
 })
+
+
+  
 message.addEventListener('keypress',()=>{
     socket.emit('typing',handle.value);
 })
@@ -26,9 +47,11 @@ socket.on('chat',(data)=>{
     feedback.innerHTML="";
     console.log(data.handle);
     output.innerHTML +='<p><strong>'+ data.handle + ':</strong>' + data.message + '</p>'
-    
-
+    // fetching initial chat messages from the database
 })
+
+
+
 
 socket.on('typing',function(data){
     feedback.innerHTML = '<p><em>'+ data + ': is typing a message...</em></p>';
